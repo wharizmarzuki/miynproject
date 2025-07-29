b638e892ff51ef57b5228254bd4c63a112a44b8a
Hariz Marzuki <hariz.marzuki@live.iium.edu.my>
2025-07-29 11:46:57 +0800

    feat: add time range to query

--- routers/query.py
+++ routers/query.py
@@ -29,11 +29,65 @@ async def query_prometheus(query: str) -> Dict[str, Any]:
     
 
 @router.get("/devices")
-async def get_all_devices():
-
+async def get_all_devices(
+    duration: str = "5m",
+    start_time: str = None, # type: ignore
+    end_time: str = None, # type: ignore
+    step: str = "15s"
+):
     try:
-        query = "{device_name=~'.+'}[5m]"
-        result = await query_prometheus(query)
+        query = f"{{device_name=~'.+'}}[{duration}]"
+        
+        if start_time and end_time:
+            # Range query with step
+            result = await query_prometheus(
+                query,
+                start=start_time, # type: ignore
+                end=end_time, # type: ignore
+                step=step # type: ignore
+            )
+        else:
+            result = await query_prometheus(query)
+        
+        print(result)
+        
+        if result["status"] != "success":
+            raise HTTPException(status_code=500, detail="Prometheus query failed")
+        
+        compiled_devices = format_metric(result)
+        
+        return {
+            "status": "success",
+            "total_devices": len(compiled_devices),
+            "devices": compiled_devices
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
+@router.get("/devices/{hostname}")
+async def get_devices(
+    hostname: str,
+    duration: str = "5m",
+    start_time: str = None, # type: ignore
+    end_time: str = None, # type: ignore
+    step: str = "15s"
+):
+    try:
+        query = f"{{device_name='{hostname}'}}[{duration}]"
+        
+        if start_time and end_time:
+            # Range query with step
+            result = await query_prometheus(
+                query,
+                start=start_time, # type: ignore
+                end=end_time, # type: ignore
+                step=step # type: ignore
+            )
+        else:
+            result = await query_prometheus(query)
+        
         print(result)
         
         if result["status"] != "success":
